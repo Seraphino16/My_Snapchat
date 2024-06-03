0813a8fa3ccea73e2c0c10210deeefe61fa0ae8c
Séraphin <seraphinbnt@gmail.com>
2024-06-03 12:53:19 +0200

    add image picker page -> import photo from gallery

---
+++ app/imagePickerPage.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Button, Image, View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ImagePickerPage() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {!image && <Button title="Pick an image from camera roll" onPress={pickImage} />}
+      {image && <Image source={{ uri: image }} style={styles.image} resizeMode="contain"/>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 300,
+    height: 1000,
+  },
+});
