6daa7161d10dd1a8a1d3b5194040a9888208e8b0
stephpiquette <svaillant.e2c@gmail.com>
2024-06-07 15:49:47 +0200

    feat: changed home page to a camera page, with a flip camera button

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -1,63 +1,84 @@
-import { Image, StyleSheet, Platform, View } from "react-native";
-import { Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import logo from "../../assets/images/logosnap.png";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import {
+    Button,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+} from "react-native";
 
 export default function HomeScreen() {
-    const navigation = useNavigation();
+    const [facing, setFacing] = useState("back");
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: "center" }}>
+                    We need your permission to show the camera
+                </Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing((current) => (current === "back" ? "front" : "back"));
+    }
 
     return (
-        <ParallaxScrollView
-            headerBackgroundColor={{ light: "yellow", dark: "#1D3D47" }}
-            headerImage={
-                <View>
-                    <Image source={logo} style={{ width: 80, height: 80, top: 40, alignSelf: 'center' }} />
-                    <Image
-                        source={require("@/assets/images/devilbg.png")}
-                        style={{
-                            width: "100%",
-                            height: 100,
-                            top: "190%",
-                            alignSelf: 'center',
-                            resizeMode: "cover",
-                            position: "absolute",
-                        }}
-                    />
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.flipButtonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={toggleCameraFacing}
+                    >
+                        <Image
+                            style={styles.icon}
+                            source={require("@/assets/images/flip.png")}
+                        />
+                    </TouchableOpacity>
                 </View>
-            }
-        >
-            <View
-                style={{ flex: 1, backgroundColor: "rgba(255, 255, 255, 0.1)" }}
-            >
-                <ThemedView style={styles.titleContainer}>
-                    <View>
-                        <ThemedText type="title">Welcome to the home page !</ThemedText>
-                    </View>
-                    <HelloWave />
-                </ThemedView>
-                <View>
-                    <ThemedText>
-                        {" "}
-                        This is the home page !{" "}
-                    </ThemedText>
-                </View>
-            </View>
-        </ParallaxScrollView>
+            </CameraView>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: "column",
-        alignItems: "center",
-        gap: 8,
+    container: {
+        flex: 1,
+        justifyContent: "center",
     },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    }
+    camera: {
+        flex: 1,
+    },
+    flipButtonContainer: {
+        position: "absolute",
+        bottom: 25,
+        flexDirection: "row",
+        backgroundColor: "transparent",
+        left: 240,
+    },
+    button: {
+        alignSelf: "flex-end",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+    },
+    icon: {
+        alignItems: "center",
+        justifyContent: "center",
+        width: 50,
+        height: 50,
+    },
 });
