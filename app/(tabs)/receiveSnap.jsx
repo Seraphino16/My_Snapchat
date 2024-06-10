7a4d27c15b21e13311ce6f5843d8e205758ca09d
charles <charles.fotie@epitech.eu>
2024-06-10 16:30:09 +0200

    feat: receivedsnap

---
+++ app/(tabs)/receiveSnap.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { ThemedView } from '@/components/ThemedView';
+import useToken from '@/hooks/useToken';
+
+
+
+export default function ReceivedImagesPage() {
+  const [images, setImages] = useState([]);
+  const {token} = useToken();
+
+
+  useEffect(() => {
+    // console.log("ethan", token);
+
+    fetch('https://snapchat.epidoc.eu/snap', { 
+
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNoYXJsZXMuZm90aWVAZXBpdGVjaC5ldSIsImlhdCI6MTcxNzc3ODc2NX0.Mmwlkue_haKhyXf_WGvkWLu2P2LxWHNHcUlA-6-ls2A', // Remplacez par votre clé API
+        'authorization': 'bearer ' + token // Assurez-vous d'avoir le token d'utilisateur
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.images) {
+        setImages(data.images);
+      } else {
+        console.error('data.images is undefined');
+
+      }
+    })
+    .catch((error) => console.error(error));
+}, [token]);
+
+  return(
+    <ThemedView>
+      {images.map((images, index) => (
+        <img key={index} src={images.url} alt="Received" /> // Remplacez par le champ correct pour l'URL de l'image
+      ))}
+    </ThemedView>
+  );
+
+}
