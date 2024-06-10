18372a5c3da78a1707fec9f9000e186e931f31e6
Séraphin <seraphinbnt@gmail.com>
2024-06-10 18:55:48 +0200

    update take phto -> now can send photo to user

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -7,15 +7,19 @@ import {
     TouchableOpacity,
     View,
     Image,
+    Pressable,
 } from "react-native";
 import { FontAwesome5 } from '@expo/vector-icons';
-import { Link } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 
 export default function HomeScreen() {
     const [facing, setFacing] = useState("back");
     const [permission, requestPermission] = useCameraPermissions();
     const [photo, setPhoto] = useState(null);
     const cameraRef = useRef(null);
+    const [isPressed, setIsPressed] = useState(false);
+    const [selectedTime, setSelectedTime] = useState(5);
+    const navigation = useNavigation();
 
     const getMimeType = (uri) => {
         const format = uri.split('.').pop();
@@ -29,7 +33,12 @@ export default function HomeScreen() {
           default:
             return false;
         }
-      }
+    }
+
+    const navigateToListUsers = () => {
+        const image = photo;
+        navigation.navigate('listUsers', { image, selectedTime });
+    };
 
     if (!permission) {
         return <View />;
@@ -63,12 +72,36 @@ export default function HomeScreen() {
                 image.base64 = `data:${mimeType};base64,${image.base64}`;
 
                 console.log(image.base64.substring(0, 50));
-                // console.log(image)
                 setPhoto(image)
             }
         } catch (error) {
             console.error(error);
         }
+    }
+
+    if (photo) {
+        console.log(photo.uri);
+        return(
+            <View style={styles.container} >
+                <Image
+                    source={{ uri: photo.uri }}
+                    style={styles.photo}
+                    resizeMode="contain"
+                />
+                <View style={styles.sendButtonContainer} >
+                <Pressable
+                    style={[styles.confirmButton, isPressed && styles.confirmButtonPressed]}
+                    onPressIn={() => setIsPressed(true)}
+                    onPressOut={() => setIsPressed(false)}
+                    onPress={() => {
+                        navigateToListUsers();
+                    }}
+                >
+                    <Text style={styles.buttonText}>Confirm</Text>
+                </Pressable>
+                </View>
+            </View>
+        )
     }
 
     return (
@@ -184,4 +217,33 @@ const styles = StyleSheet.create({
         width: 60,
         height: 60,
     },
+    photo: {
+        width: '100%',
+        height: '100%',
+    },
+    confirmButton: {
+        position: 'absolute',
+        bottom: 32,
+        right: -12,
+        transform: [{ translateX: -50 }],
+        zIndex: 1,
+        backgroundColor: '#13bceb',
+        width: 100,
+        paddingVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        transitionDuration: 200,
+        transitionProperty: 'background-color',
+        transitoinTimingFunction: 'ease-in-out',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    confirmButtonPressed: {
+        backgroundColor: '#1c9abd'
+    },
+    buttonText: {
+        color: 'white',
+        marginRight: 12,
+    }
 });
