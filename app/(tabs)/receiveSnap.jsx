ba7aba83bbd2495cfbf8be22e43427406b9fa4bf
Séraphin <seraphinbnt@gmail.com>
2024-06-11 12:45:51 +0200

    update snap list -> get Name of users and improve design

--- app/(tabs)/receiveSnap.jsx
+++ app/(tabs)/receiveSnap.jsx
@@ -1,44 +1,122 @@
 import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import useToken from '@/hooks/useToken';
+import { ThemedText } from '@/components/ThemedText';
 
+function SnapListItem ({ snap, token }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser () {
+      try {
+        const response = await fetch(`https://snapchat.epidoc.eu/user/${snap.from}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNoYXJsZXMuZm90aWVAZXBpdGVjaC5ldSIsImlhdCI6MTcxNzc3ODc2NX0.Mmwlkue_haKhyXf_WGvkWLu2P2LxWHNHcUlA-6-ls2A', // Remplacez par votre clé API
+            'authorization': 'bearer ' + token
+          },
+        })
+        const data = await response.json();
+        setUser(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    fetchUser();
+    }, [snap]);
+
+  if(user) {
+    console.log(user);
+  }
+
+
+  return(
+    <ThemedView style={styles.snapItemContainer}>
+      {user && (
+        <ThemedView>
+          <ThemedText>{user.username}</ThemedText>
+          <ThemedView style={styles.newSnapContainer}>
+            <View style={styles.square}></View>
+            <ThemedText style={styles.redText}>Nouveau snap</ThemedText>
+          </ThemedView>
+        </ThemedView>
+      )}
+      
+    </ThemedView>
+  )
+}
 
 
 export default function ReceivedImagesPage() {
-  const [images, setImages] = useState([]);
+  const [snaps, setSnaps] = useState(null);
   const {token} = useToken();
 
-
   useEffect(() => {
-    // console.log("ethan", token);
-
-    fetch('https://snapchat.epidoc.eu/snap', { 
+    
+    if(token) {
+      fetch('https://snapchat.epidoc.eu/snap', { 
 
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNoYXJsZXMuZm90aWVAZXBpdGVjaC5ldSIsImlhdCI6MTcxNzc3ODc2NX0.Mmwlkue_haKhyXf_WGvkWLu2P2LxWHNHcUlA-6-ls2A', // Remplacez par votre clé API
-        'authorization': 'bearer ' + token // Assurez-vous d'avoir le token d'utilisateur
+        'authorization': 'bearer ' + token
       },
     })
     .then((response) => response.json())
     .then((data) => {
-      if (data.id) {
-        setImages(data.id);
-      } else {
-        console.error(data);
-
-      }
+      setSnaps(data.data);
     })
     .catch((error) => console.error(error));
-}, [token]);
+    }
+    
+  }, [token]);
 
   return(
     <ThemedView>
-      {images.map((images, index) => (
-        <img key={index} src={images.url} alt="Received" /> // Remplacez par le champ correct pour l'URL de l'image
+      <ThemedView style={styles.header}>
+        <ThemedText style={styles.title}>Chat</ThemedText>
+      </ThemedView>
+      {snaps && snaps.map((snap) => (
+        <SnapListItem snap={snap} token={token} />
       ))}
     </ThemedView>
   );
 
 }
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 100,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 30,
+    fontSize: 20,
+  },
+  snapItemContainer: {
+    padding: 12,
+  },
+  newSnapContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  square: {
+    width: 16,
+    height: 16,
+    backgroundColor: 'red',
+    borderRadius: 4,
+  },
+  redText: {
+    color: 'red',
+    marginLeft: 8,
+  },
+
+})
