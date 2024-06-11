3f3e377af169e8af4f52a247040f218a53b3e568
stephpiquette <svaillant.e2c@gmail.com>
2024-06-11 23:02:07 +0200

    fix: fixed keyboard getting over forms

--- app/(login)/login.jsx
+++ app/(login)/login.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button, Image } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Button,
+    Image,
+    Platform,
+    KeyboardAvoidingView,
+} from "react-native";
 import { Link, useNavigation } from "@react-navigation/native";
 import BootstrapStyleSheet from "react-native-bootstrap-styles";
 import logo from "../../assets/images/logosnap.png";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import useToken from "@/hooks/useToken";
-
 
 export default function Login() {
     const navigation = useNavigation();
@@ -24,7 +32,8 @@ export default function Login() {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
-                "x-api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlcmFwaGluLmJlbm9pdEBlcGl0ZWNoLmV1IiwiaWF0IjoxNzE3NzYzMDg5fQ.yVGQmbarWgv25YxWcwl01igKET7stSAfJ4eRvmaTvrU"
+                "x-api-key":
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlcmFwaGluLmJlbm9pdEBlcGl0ZWNoLmV1IiwiaWF0IjoxNzE3NzYzMDg5fQ.yVGQmbarWgv25YxWcwl01igKET7stSAfJ4eRvmaTvrU",
             },
             body: JSON.stringify({
                 email: email,
@@ -33,14 +42,13 @@ export default function Login() {
         })
             .then((response) => response.json())
             .then(async (data) => {
-                
                 try {
                     // Assure que le token est dans l'objet data
-                    if(data.data.token) {
+                    if (data.data.token) {
                         saveToken(data.data.token);
-                        router.replace('(tabs)');
+                        router.replace("(tabs)");
                     } else {
-                        console.log('Error with connexion');
+                        console.log("Error with connexion");
                     }
                 } catch (e) {
                     console.error("Erreur de sauvegarde du token:", e);
@@ -52,66 +60,86 @@ export default function Login() {
             });
     };
 
-
-
     return (
-        <View style={[s.container, { justifyContent: "center", flex: 1 }]}>
-            <ThemedView style={[styles.titleContainer, { backgroundColor: 'transparent' }]}>
-                <View>
-                    <ThemedText type="title">Login !</ThemedText>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 50 : 100}
+        >
+            <View style={[s.container, { justifyContent: "center", flex: 1 }]}>
+                <ThemedView
+                    style={[
+                        styles.titleContainer,
+                        { backgroundColor: "transparent" },
+                    ]}
+                >
+                    <View>
+                        <ThemedText type="title">Login !</ThemedText>
+                    </View>
+                </ThemedView>
+                <View
+                    style={[
+                        s.container,
+                        {
+                            alignItems: "center",
+                            justifyContent: "center",
+                            flex: 0.25,
+                        },
+                    ]}
+                >
+                    <Image source={logo} style={[{ width: 80, height: 80 }]} />
                 </View>
-            </ThemedView>
-            <View
-                style={[
-                    s.container,
-                    {
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flex: 0.25,
-                    },
-                ]}
-            >
-                <Image source={logo} style={[{ width: 80, height: 80 }]} />
-            </View>
-            <Text style={[s.text, { color: c.danger }]}>Email:</Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={email}
-                onChangeText={setEmail}
-            />
-    
-            <Text style={[s.text, s.mt4, { color: c.danger }]}>Password:</Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-    
-            <View style={s.mt4}>
-                <Button title="Login" onPress={handleLogin} />
-            </View>
-            <Link style={styles.link} to='/(login)/signup' >You don't have an account ? Sign up here</Link>
-        </View>
-    );
+                <Text style={[s.text, { color: c.danger }]}>Email:</Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={email}
+                    onChangeText={setEmail}
+                />
 
-    }
-    const styles = StyleSheet.create({
-        titleContainer: {
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 8,
-        },
-        link: {
-            textAlign: 'center',
-            textDecorationLine: 'underline',
-        }
-    });
+                <Text style={[s.text, s.mt4, { color: c.danger }]}>
+                    Password:
+                </Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+
+                <View style={s.mt4}>
+                    <Button title="Login" onPress={handleLogin} />
+                </View>
+                <Link style={styles.link} to="/(login)/signup">
+                    You don't have an account ? Sign up here
+                </Link>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+const styles = StyleSheet.create({
+    titleContainer: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 8,
+    },
+    link: {
+        textAlign: "center",
+        textDecorationLine: "underline",
+    },
+});
--- app/(login)/signup.jsx
+++ app/(login)/signup.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet,TextInput, Button, Image } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    KeyboardAvoidingView,
+    Button,
+    Platform,
+    Image,
+} from "react-native";
 import { Link, useNavigation } from "@react-navigation/native";
 import BootstrapStyleSheet from "react-native-bootstrap-styles";
 import logo from "../../assets/images/logosnap.png";
@@ -11,7 +20,6 @@ import useToken from "@/hooks/useToken";
 export default function SignUp() {
     const navigation = useNavigation();
 
-    
     const [username, setUsername] = useState("");
     const [profilePicture, setProfilePicture] = useState("");
     const [email, setEmail] = useState("");
@@ -26,7 +34,8 @@ export default function SignUp() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "x-api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlcmFwaGluLmJlbm9pdEBlcGl0ZWNoLmV1IiwiaWF0IjoxNzE3NzYzMDg5fQ.yVGQmbarWgv25YxWcwl01igKET7stSAfJ4eRvmaTvrU"
+                "x-api-key":
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlcmFwaGluLmJlbm9pdEBlcGl0ZWNoLmV1IiwiaWF0IjoxNzE3NzYzMDg5fQ.yVGQmbarWgv25YxWcwl01igKET7stSAfJ4eRvmaTvrU",
             },
             body: JSON.stringify({
                 username: username,
@@ -37,10 +46,8 @@ export default function SignUp() {
         })
             .then((response) => response.json())
             .then((data) => {
-                if(data._id) {
-                    fetchToken(email, password)
-                        .then(router.replace('(tabs)'));
-                    
+                if (data._id) {
+                    fetchToken(email, password).then(router.replace("(tabs)"));
                 }
                 console.log(data);
             })
@@ -50,87 +57,117 @@ export default function SignUp() {
     };
 
     return (
-        <View style={[s.container, { justifyContent: "center", flex: 1 }]}>
-          <ThemedView style={[styles.titleContainer, { backgroundColor: 'transparent' }]}>
-                <View>
-                    <ThemedText type="title">Sign up !</ThemedText>
+        <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 50 : 100}>
+            <View style={[s.container, { justifyContent: "center", flex: 1 }]}>
+                <ThemedView
+                    style={[
+                        styles.titleContainer,
+                        { backgroundColor: "transparent" },
+                    ]}
+                >
+                    <View>
+                        <ThemedText type="title">Sign up !</ThemedText>
+                    </View>
+                </ThemedView>
+                <View
+                    style={[
+                        s.container,
+                        {
+                            alignItems: "center",
+                            justifyContent: "center",
+                            flex: 0.35,
+                        },
+                    ]}
+                >
+                    <Image source={logo} style={[{ width: 80, height: 80 }]} />
                 </View>
-            </ThemedView>
-            <View
-                style={[
-                    s.container,
-                    {
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flex: 0.35,
-                    },
-                ]}
-            >
-                <Image source={logo} style={[{ width: 80, height: 80 }]} />
+                <Text style={[s.text, { color: c.danger }]}>Username:</Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={username}
+                    onChangeText={setUsername}
+                />
+
+                <Text style={[s.text, s.mt4, { color: c.danger }]}>
+                    Profile Picture:
+                </Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={profilePicture}
+                    onChangeText={setProfilePicture}
+                />
+
+                <Text style={[s.text, s.mt4, { color: c.danger }]}>Email:</Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={email}
+                    onChangeText={setEmail}
+                />
+
+                <Text style={[s.text, s.mt4, { color: c.danger }]}>
+                    Password:
+                </Text>
+                <TextInput
+                    style={[
+                        s.border,
+                        s.p1,
+                        {
+                            minWidth: "80%",
+                            height: 40,
+                            backgroundColor: "white",
+                        },
+                    ]}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+
+                <View style={s.mt4}>
+                    <Button title="SignUp" onPress={handleSignUp} />
+                </View>
+                <Link style={styles.link} to="/(login)/login">
+                    You don't have an account ? Sign up here
+                </Link>
             </View>
-            <Text style={[s.text, { color: c.danger }]}>Username:</Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={username}
-                onChangeText={setUsername}
-            />
-
-            <Text style={[s.text, s.mt4, { color: c.danger }]}>
-                Profile Picture:
-            </Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={profilePicture}
-                onChangeText={setProfilePicture}
-            />
-
-            <Text style={[s.text, s.mt4, { color: c.danger }]}>Email:</Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={email}
-                onChangeText={setEmail}
-            />
-
-            <Text style={[s.text, s.mt4, { color: c.danger }]}>Password:</Text>
-            <TextInput
-                style={[
-                    s.border,
-                    s.p1,
-                    { minWidth: "80%", height: 40, backgroundColor: "white" },
-                ]}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-
-            <View style={s.mt4}>
-                <Button title="SignUp" onPress={handleSignUp} />
-            </View>
-            <Link style={styles.link} to='/(login)/login' >You don't have an account ? Sign up here</Link>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-      flexDirection: "column",
-      alignItems: "center",
-      gap: 8,
-  },
-  link: {
-    textAlign: 'center',
-    textDecorationLine: 'underline',
-  }
+    titleContainer: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 8,
+    },
+    link: {
+        textAlign: "center",
+        textDecorationLine: "underline",
+    },
 });
